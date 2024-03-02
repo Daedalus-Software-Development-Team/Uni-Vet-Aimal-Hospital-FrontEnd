fdb04458ac02e72063f8fa74293ed467ffc200b7
Universal99OS <pandukapiushanpullaperuma4321@gmail.com>
2024-03-02 15:26:20 +0530

    customer login

--- uni-vet-app/src/Componnent/Customerlogin/Customerlogin.jsx
+++ uni-vet-app/src/Componnent/Customerlogin/Customerlogin.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import React, { useState, } from 'react';
+
 function Customerlogin() {
 
     const customer = {
@@ -9,9 +11,36 @@ function Customerlogin() {
         nicNum: null
     }
 
+    const [firstNameC, setFirstName] = useState("");
+    const [lastNameC, setLastName] = useState("");
+    const [emailC, setEmail] = useState("");
+    const [nicNumC, setNic] = useState("");
+
+    function handleFirstName(event) {
+        setFirstName(event.target.value);
+    }
+
+    function handleLastName(event) {
+        setLastName(event.target.value);
+    }
+
+    function handleEmail(event) {
+        setEmail(event.target.value);
+    }
+
+    function handleNic(event) {
+        setNic(event.target.value);
+    }
+
+    const cleanForm = () => {
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setNic("");
+    }
+
     const handlePostRequest = async () => {
         try {
-
 
             const apiUrl = "http://localhost:8080/customer";
 
@@ -19,16 +48,23 @@ function Customerlogin() {
 
             console.log("Succefull", responce);
 
+            cleanForm();
+
+
+
         } catch (error) {
             console.log(error);
         }
     }
 
     function submitOnHandle() {
-        customer.firstName = firstName.value;
-        customer.lastName = lastName.value;
-        customer.email = email.value;
-        customer.nicNum = nic.value;
+
+
+        customer.firstName = firstNameC;
+        customer.lastName = lastNameC;
+        customer.email = emailC;
+        customer.nicNum = nicNumC;
+
 
         console.log(customer);
         handlePostRequest();
@@ -50,13 +86,13 @@ function Customerlogin() {
                         <hr />
                     </div>
                     <label class="form-label student-label">First Name</label>
-                    <input type="text" id="firstName" class="form-control mb-2" />
+                    <input value={firstNameC} onChange={handleFirstName} type="text" id="firstName" class="form-control input-Field mb-2" />
                     <label class="form-label student-label">Last Name</label>
-                    <input type="text" id="lastName" class="form-control mb-2" />
+                    <input value={lastNameC} onChange={handleLastName} type="text" id="lastName" class="form-control input-Field mb-2" />
                     <label class="form-label student-label">Email</label>
-                    <input type="text" id="email" class="form-control mb-2" />
+                    <input value={emailC} onChange={handleEmail} type="text" id="email" class="form-control input-Field mb-2" />
                     <label class="form-label student-label">NIC Number</label>
-                    <input type="number" id="nic" class="form-control" />
+                    <input value={nicNumC} onChange={handleNic} type="number" id="nic" class="form-control input-Field" />
                     <div className="buttonFrame">
                         <button onClick={submitOnHandle} type="submit" class="btn btn-primary mt-3 submit-button">Submit</button>
                     </div>
